a01f93eb3f21a839b4026ca2b75f3fc23392f80e
Chingiz Mizambekov <chinjka_m@hotmail.com>
2017-03-15 09:09:44 +0000

    Labo1: blink with ON/OFF switch finished

---
+++ labo1/blink.py
@@ -0,0 +1,35 @@
+
+#Import modules
+import RPi.GPIO as GPIO
+import time
+
+#Define variables
+buttonPin = 17
+ledPin = 18
+ledOn = False
+previous_input = 0
+
+
+#Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(ledPin, GPIO.OUT)
+
+
+#Loop
+while (True):
+	#get current input from the button
+	input = GPIO.input(buttonPin)
+
+	#if button is pressed check previous button state
+	if ((not previous_input) and input):		
+		#turn the led ON/OFF
+		GPIO.output(ledPin, ledOn)
+		ledOn = not ledOn
+	
+	#Save last button state
+	previous_input = input
+
+	#Wait some tome to debounce the button
+	time.sleep(0.05)
+	
